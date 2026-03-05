c76dce233aa4b5e5a42616463fd059f28a689ffa
ayush619 <ayush.anand8400@gmail.com>
2026-03-05 20:02:59 +0530

    till journal entry controller

---
+++ src/main/java/com/ffi/edigest/EdigestApplication.java
@@ -0,0 +1,13 @@
+package com.ffi.edigest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdigestApplication.class, args);
+	}
+
+}
